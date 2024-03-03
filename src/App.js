0bb77b62b6a13157ecf794b5045d0a6009687a8d
Atharva Lakhamade <atharva19510@gmail.com>
2024-03-04 00:24:27 +0530

    Add homepage and deploy script

--- src/App.js
+++ src/App.js
@@ -1,48 +1,38 @@
+import React, { useState } from "react";
+import profile from "./images/profile.jpg";
 
-
-
-import React, { useState } from 'react'
-import profile from './images/profile.jpg';
-
-import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './TextForm';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./TextForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  const [mode,setMode] = useState('light');
-  const toggleMode = ()=>{
-      if(mode === 'light'){
-        setMode('dark')
-        document.body.style.backgroundColor = "#082844"
-      }else{
-        setMode('light')
-        document.body.style.backgroundColor = "white"
-      }
-     
-  }
-  return ( 
-    
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#082844";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+  return (
     <>
       <BrowserRouter>
-      <Navbar logo="Text Analyser" mode={mode} toggleMode={toggleMode}/>
-      {/* <TextForm heading="Enter the text to analyse: " mode={mode}/> */}
-         <Routes>
-          <Route exact path="/about" element={<About imgSrc={profile}/>} /> 
+        <Navbar logo="TextConvert" mode={mode} toggleMode={toggleMode} />
+        {/* <TextForm heading="Enter the text to analyse: " mode={mode}/> */}
+        <Routes>
+          <Route exact path="/about" element={<About imgSrc={profile} />} />
           {/* exact is usedto get exact path sometimes there are folders like /about-->componentX and /about/home-->componentY and if we want component from /about/home it will mistakenly will render from /about thats why */}
-          
-          < Route exact path="/"
-            element= {<TextForm heading="Enter the text to analyse: " mode={mode}/>} /> 
-          </Routes>
-          </BrowserRouter>
-    
-    
 
+          <Route
+            exact
+            path="/"
+            element={<TextForm heading="Enter the text here ↓ " mode={mode} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
